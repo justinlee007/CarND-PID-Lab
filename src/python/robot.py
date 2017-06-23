2256fe12b51ffcbe2686bce65124f56e392877fa
Justin Lee <justin.lee007@gmail.com>
2017-06-22 18:09:53 -0700

    Update robot to plot when run in pycharm

--- src/python/robot.py
+++ src/python/robot.py
@@ -16,6 +16,9 @@
 
 import random
 
+import matplotlib
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -123,7 +126,6 @@ def run(robot, params, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
     err = 0
-    # TODO: your code here
     prev_cte = robot.y
     int_cte = 0
     for i in range(2 * n):
@@ -170,11 +172,11 @@ def twiddle(tol=0.2):
                     p[i] += dp[i]
                     dp[i] *= 0.9
         it += 1
-    return p
+    return p, best_err
 
 
 params, err = twiddle()
-print("Final twiddle error = {}".format(err))
+print("Final twiddle error={}, params={}, {}, {}".format(err, params[0], params[1], params[2]))
 robot = make_robot()
 x_trajectory, y_trajectory, err = run(robot, params)
 n = len(x_trajectory)
@@ -182,3 +184,4 @@ n = len(x_trajectory)
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
 ax1.plot(x_trajectory, y_trajectory, 'g', label='Twiddle PID controller')
 ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.show()
